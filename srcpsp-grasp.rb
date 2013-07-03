b9b59723174a3b87a256407531a4b82a80cce78a
Sebastian Klepper <sklppr@users.noreply.github.com>
2013-07-03 23:16:50 +0200

    Ensure compatibility with MRI 1.9.x and JRuby.

--- srcpsp-grasp.rb
+++ srcpsp-grasp.rb
@@ -1,4 +1,6 @@
-$:.unshift __dir__
+# encoding: utf-8
+
+$:.unshift File.dirname(__FILE__)
 
 require "srcpsp-grasp/project"
 require "srcpsp-grasp/solution"
--- srcpsp-grasp/solution.rb
+++ srcpsp-grasp/solution.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 module SRCPSP_GRASP
   
   class Solution
