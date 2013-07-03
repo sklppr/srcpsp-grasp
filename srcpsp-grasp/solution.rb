c107484e835b71fd9eb9da447505833e0fac7ae7
Sebastian Klepper <sk@sebastianklepper.com>
2013-07-03 18:52:10 +0200

    Implement schedule generation.

--- srcpsp-grasp/solution.rb
+++ srcpsp-grasp/solution.rb
@@ -15,16 +15,80 @@ module SRCPSP_GRASP
       @activities << activity
     end
   
-    # Generates schedule from activity list and calculates makespan.
-    def evaluate!
-      # @TODO
-    end
-  
     # Returns solution with inverted activity list.
     def invert
       solution = self.clone
       solution.activities.reverse!
       solution
+    end
+
+    # Generates schedule from activity list using a serial SGS.
+    def evaluate!
+
+      # Clone the project's resources to be able to modify their capacity.
+      resources = @project.resources.clone
+
+      # Keep track of scheduled activities.
+      scheduled_activities
+
+      # The generated schedule will contain start times with activity IDs as indices.
+      schedule = []
+
+      # Iterate through remaining activities, including dummy sink.
+      @activities.each do |activity|
+
+        # Determine earliest start of activity:
+        # 1. ES <= latest finish of all predecessors. (default rule of serial SGS)
+        time = activity.predecessors.collect do |predecessor_id|
+          schedule[predecessor_id] + @project.activities[predecessor_id].duration
+        end.max || 0 # If activity has no predecessors, its earliest start is 0.
+        # 2. ES must be <= latest start of all scheduled activities. (activitiy-based priority rule)
+        time = [time, (schedule.max || 0)].max # If schedule is empty, the latest start is 0.
+
+        # Next, determine point in time where the activity is resource feasible.
+        # The way this solution was generated already ensures time feasibility.
+
+        # Start by testing wether it's already feasible. Otherwise ...
+        unless activity_is_resource_feasible?(scheduled_activities, schedule, project, time)
+
+          # Collect finish times of ongoing activities, sorted ascending.
+          finish_times = scheduled_activities.select do |activity|
+            schedule[activity.id] <= time && time < schedule[activity.id] + activity.duration
+          end.collect do |activity|
+            schedule[activity.id] + activity.duration
+          end.sort
+
+          # Test feasibility at each finish time.
+          finish_times.each do |finish_time|
+            time = finish_time and break if activity_is_resource_feasible?(schedule, project, finish_time)
+          end
+
+        end
+
+        # Schedule activity at the resulting point in time.
+        schedule[activity.id] = time
+        scheduled_activities << activity
+
+      end
+
+      # Set makespan.
+      @makespan = schedule[@activities.last.id]
+
+    end
+
+    # Tests if an activity is resource feasible at a certain point in time.
+    def activity_is_resource_feasible?(scheduled_activities, schedule, project, time)
+
+      # Determine ongoing activities and add potential activity.
+      activities = scheduled_activities.select do |activity|
+        schedule[activity.id] <= time && time < schedule[activity.id] + activity.duration
+      end << activity
+
+      # Return wether capacity of each resource is >= sum of resource usage by all activities.
+      project.resources.all? do |resource|
+        resource.capacity >= activities.inject(0) { |sum, activity| sum + activity.resource_usage[resource.id] }
+      end
+
     end
   
   end
