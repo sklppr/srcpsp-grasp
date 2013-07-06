224cc2104ed20be0daf98bfa550a7bd54f5d6e66
Sebastian Klepper <sk@sebastianklepper.com>
2013-07-07 01:51:02 +0200

    Add calculation of metrics.

--- srcpsp-grasp.rb
+++ srcpsp-grasp.rb
@@ -2,17 +2,24 @@
 
 $:.unshift File.dirname(__FILE__)
 
-require "srcpsp-grasp/solver"
+require "srcpsp-grasp/analyzer"
+require "srcpsp-grasp/concurrency"
+require "srcpsp-grasp/distribution"
 require "srcpsp-grasp/project"
 require "srcpsp-grasp/solution"
-require "srcpsp-grasp/distribution"
-require "srcpsp-grasp/concurrency"
+require "srcpsp-grasp/solver"
 
 module SRCPSP_GRASP
   
-  # Shortcut to read project from a file and solve it.
+  # Reads project from file and solves it.
   def self.solve(file, options={})
     Solver.new(options).solve(Project.from_file(file))
   end
+
+  # Reads project from file and analyzes it.
+  def self.analyze(file, metrics=Analyzer::METRICS)
+    Analyzer.new(Project.from_file(file)).analyze(metrics)
+  end
+
   
 end
