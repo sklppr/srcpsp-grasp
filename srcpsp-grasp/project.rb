c0ee457f3528e498b94621c308de386eb0576be1
Sebastian Klepper <sklppr@users.noreply.github.com>
2013-07-07 12:16:35 +0200

    Remove excess whitespace.

--- srcpsp-grasp/project.rb
+++ srcpsp-grasp/project.rb
@@ -21,34 +21,27 @@ module SRCPSP_GRASP
     
     # Calculates earliest/latest start/finish times using the Triple algorithm.
     def calculate_start_and_finish_times!
-
       # Initialize distance matrix with negative infinity and 0 in the diagonal.
       n = @activities.size
       distance = Array.new(n) { |i| Array.new(n) { |j| i == j ? 0 : -Float::INFINITY } }
-      
       # For all precedences (i,j) set the distance to the duration of i.
       @activities.each do |activity|
         activity.successors.each do |successor|
           distance[activity.id][successor.id] = activity.duration
         end
       end
-      
       # For all activities (use IDs):
       activities = @activities.collect(&:id)
       activities.each do |a|
-
         # For all pairs (i,j) not including a ...
         ij = activities - [a]
         ij.each do |i|
           # ... and a cost d[i,a] > negative infinity:
           next if distance[i][a] == -Float::INFINITY
-
           # If the path i->a->j is longer than i->j until now, set i->a->j as new distance between i and j.
           ij.each { |j| distance[i][j] = [ distance[i][j], distance[i][a] + distance[a][j] ].max }
-
         end
       end
-      
       # Set earliest/latest start/finish times of each activity.
       @activities.each do |a|
         a.earliest_start = distance[0][a.id]
@@ -56,32 +49,25 @@ module SRCPSP_GRASP
         a.earliest_finish = a.earliest_start + a.duration
         a.latest_finish = a.latest_start + a.duration
       end
-
     end
 
     # Reads project from a PSPLIB file (.RCP).
     def self.from_file(file)
-      
       # Create project.
       project = Project.new
-      
       # Read file.
       File.open(file, "r") do |file|
-        
         # First line contains number of activities and resources.
         line = file.gets.split
         n_activities = line[0].to_i
         n_resources = line[1].to_i
-        
         # Second line contains resource availability.
         line = file.gets.split
         n_resources.times do |k|
           project.resources << Resource.new(k, line[k].to_i)
         end
-        
         # The next remaining lines contain activities.
         n_activities.times do |i|
-          
           # Format: duration [usage]{n_resources} n_successors [successor]{n_successors}
           line = file.gets.split
           duration = line[0].to_i
@@ -89,25 +75,19 @@ module SRCPSP_GRASP
           n_successors = line[n_resources + 1].to_i
           # Fix successor IDs by subtracting 1 so that they correspond to array indices.
           successors = line[(n_resources+2)..(n_resources+1+n_successors)].map { |j| j.to_i - 1 }
-          
           # Add new activity to project, leave predecessors empty for now.
           project.activities << Activity.new(i, duration, resource_usage, [], successors)
-        
         end
-
         # Replace successor indices with references.
         project.activities.each do |activity|
           activity.successors = activity.successors.map { |successor_id| project.activities[successor_id] }
         end
-
         # Add activities as predecessor to their successors.
         project.activities.each do |activity|
           activity.successors.each { |successor| project.activities[successor.id].predecessors << activity }
         end
-
         # Return project.
         project
-        
       end
       
     end
--- srcpsp-grasp/solution.rb
+++ srcpsp-grasp/solution.rb
@@ -44,36 +44,26 @@ module SRCPSP_GRASP
 
     # Calculates expected makespan by generating a schedule from activity list (serial SGS).
     def calculate_expected_makespan(n_replications, distribution)
-
       # Collect makespans for given number of replications.
       makespans = []
       n_replications.times do
-
         # Draw activity durations from given distribution.
         activity_durations = @activities.collect { |activity| Distribution.send(@distribution, activity.duration) }
-
         # Calculate makespan for these activity durations.
         makespans << calculate_makespan(activity_durations)
-
       end
-
       # Set expected makespan to be the average of collected makespans.
       @expected_makespan = makespans.inject(:+) / makespans.size
-
     end
 
     # Calculates makespan for given activity durations by generating a schedule from activity list (serial SGS).
     def calculate_makespan(activity_durations)
-
       # Keep track of scheduled activities.
       scheduled_activities = []
-
       # The generated schedule will contain start times with activity IDs as indices.
       schedule = []
-
       # Iterate through remaining activities, including dummy sink.
       @activities.each do |activity|
-
         # Determine earliest start of activity:
         # 1. ES >= latest finish of all predecessors. (default rule of serial SGS)
         # 2. ES must be >= latest start of all scheduled activities. (activitiy-based priority rule)
@@ -81,51 +71,39 @@ module SRCPSP_GRASP
         # 4. If schedule is empty, the latest start in the schedule is 0.
         finish_times = activity.predecessors.collect { |predecessor| schedule[predecessor.id] + activity_durations[predecessor.id] }
         time = [(finish_times.max || 0), (schedule.reject(&:nil?).max || 0)].max
-
         # Next, determine point in time where the activity is resource feasible.
         # The way this solution was generated already ensures time feasibility.
-
         # Start by testing wether it's already feasible. Otherwise ...
         unless activity_is_resource_feasible?(activity, activity_durations, schedule, scheduled_activities, time)
-
           # Collect finish times of ongoing activities, sorted ascending.
           finish_times = scheduled_activities.select do |activity|
             schedule[activity.id] <= time && time < schedule[activity.id] + activity_durations[activity.id]
           end.collect do |activity|
             schedule[activity.id] + activity_durations[activity.id]
           end.sort
-
           # Test feasibility at each finish time.
           finish_times.each do |finish_time|
             time = finish_time and break if activity_is_resource_feasible?(activity, activity_durations, schedule, scheduled_activities, time)
           end
-
         end
-
         # Schedule activity at the resulting point in time.
         schedule[activity.id] = time
         scheduled_activities << activity
-
       end
-
       # Return resulting makespan.
       schedule[@activities.last.id]
-
     end
 
     # Tests if an activity is resource feasible at a certain point in time.
     def activity_is_resource_feasible?(activity, activity_durations, schedule, scheduled_activities, time)
-
       # Determine ongoing activities and add potential activity.
       activities = scheduled_activities.select do |activity|
         schedule[activity.id] <= time && time < schedule[activity.id] + activity_durations[activity.id]
       end << activity
-
       # Return wether capacity of each resource is >= sum of resource usage by all activities.
       @project.resources.all? do |resource|
         resource.capacity >= activities.inject(0) { |sum, activity| sum + activity.resource_usage[resource.id] }
       end
-
     end
   
   end
