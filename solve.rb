37c0e08aab4873d15084e988752d4bb578315548
Sebastian Klepper <sklppr@users.noreply.github.com>
2013-07-08 23:16:28 +0200

    Change evaluation method.

--- solve.rb
+++ solve.rb
@@ -3,32 +3,44 @@
 require "csv"
 require "./srcpsp-grasp"
 
-partitions = [ (0...120), (120...240), (240...360), (360...480) ]
-
+# Select input files according to partition (with size 60).
 partition = ARGV[0].to_i
-distribution = ARGV[1].to_sym
-inverse = ARGV[2] == "inverse"
+files = Dir["data/j30rcp/*.RCP"]
+files = files.slice((partition-1)*60, 60)
 
-options = {
-  distribution: distribution,
-  p_random: inverse ? 0 : 0.05,
-  p_inverse: inverse ? 0.05 : 0,
-  max_solutions: 500,
-  max_unsuccessful_solutions: 100,
+# Create solvers with different configurations.
+solvers = {
+  exponential: SRCPSP_GRASP::Solver.new({ distribution: :exponential, p_random: 0.05, max_solutions: 5000 }),
+  exponential_inverse: SRCPSP_GRASP::Solver.new({ distribution: :exponential, p_inverse: 0.05, max_solutions: 5000 }),
+  uniform: SRCPSP_GRASP::Solver.new({ distribution: :uniform_sqrt, p_random: 0.05, max_solutions: 5000 }),
+  uniform_inverse: SRCPSP_GRASP::Solver.new({ distribution: :uniform_sqrt, p_inverse: 0.05, max_solutions: 5000 })
 }
 
-output = "data/j30rcp-#{distribution}#{"-inverse" if inverse}-#{partition}.csv"
-files = Dir["data/j30rcp/*.RCP"][partitions[partition-1]]
+# Create output files and write headers.
+solvers.each do |type, solver|
 
-CSV.open(output, "wb") do |csv|
-  csv << %w[ instance makespan ]
+  CSV.open("data/j30rcp-#{type}-#{partition}.csv", "wb") do |csv|
+    csv << %w[ instance expected_makespan true_makespan ]
+  end
+
 end
 
-solver = SRCPSP_GRASP::Solver.new(options)
+# Read each input file and parse project.
 files.each do |file|
+
   project = SRCPSP_GRASP::Project.from_file(file)
-  solution = solver.solve(project)
-  CSV.open(output, "ab") do |csv|
-    csv << [ File.basename(file, ".RCP"), solution.expected_makespan ]
+
+  # Solve project with each solver, calculate and write makespans.
+  solvers.each do |type, solver|
+
+    solution = solver.solve(project)
+    expected_makespan = solution.expected_makespan
+    true_makespan = solution.true_makespan
+
+    CSV.open("data/j30rcp-#{type}-#{partition}.csv", "ab") do |csv|
+      csv << [ File.basename(file, ".RCP"), expected_makespan, true_makespan ]
+    end
+
   end
+
 end
